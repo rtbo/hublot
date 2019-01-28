a51b98bb46de25e388a459d7320a43587bb5560e
Remi Thebault <remi.thebault@gmail.com>
2019-01-28 22:34:12 +0100

    paint

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,10 @@ extern crate lazy_static;
 pub mod color;
 pub mod geom;
 pub mod gfx;
+pub mod paint;
 pub mod render;
 pub mod ui;
 
 pub use color::Color;
+pub use paint::Paint;
 pub use ui::UserInterface;
---
+++ src/paint.rs
@@ -0,0 +1,27 @@
+
+use crate::Color;
+
+pub mod gradient {
+    use crate::Color;
+
+    /// A control stop for color gradients
+    pub struct Stop (pub f32, pub Color);
+
+    /// Direction of a linear gradient
+    pub enum Direction {
+        Angle(f32),
+        N,
+        NE,
+        E,
+        SE,
+        S,
+        SW,
+        W,
+        NW,
+    }
+}
+
+pub enum Paint {
+    Solid(Color),
+    LinearGradient(Vec<gradient::Stop>, gradient::Direction),
+}
