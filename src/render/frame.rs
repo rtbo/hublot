2dada70fb6d2c6c6d4378a0149e29d8c18d574a4
Remi Thebault <remi.thebault@gmail.com>
2019-01-28 22:35:43 +0100

    framegraph

--- src/render/frame.rs
+++ src/render/frame.rs
@@ -1,15 +1,37 @@
-use crate::geom::IRect;
-use crate::Color;
+use crate::geom::{FRect, IRect};
+use crate::{Color, Paint};
 use winit::WindowId;
 
 pub struct Frame {
     pub window: WindowId,
     pub viewport: IRect,
     pub clear_color: Option<Color>,
+    pub root: Option<Node>,
 }
 
 impl Frame {
-    pub fn new (window: WindowId, viewport: IRect, clear_color: Option<Color>) -> Frame {
-        Frame { window, viewport, clear_color }
+    pub fn new(
+        window: WindowId,
+        viewport: IRect,
+        clear_color: Option<Color>,
+        root: Option<Node>,
+    ) -> Frame {
+        Frame {
+            window,
+            viewport,
+            clear_color,
+            root,
+        }
     }
+}
+
+pub enum Node {
+    Group(Vec<Node>),
+    Transform(Box<Node>, [f32; 16]),
+    Rect {
+        rect: FRect,
+        paint: Paint,
+        radius: f32,
+        border: Option<(Color, f32)>,
+    },
 }
